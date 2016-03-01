6c68dc6639bacb34525458c0d5f9832ae7cef07d
Wiktor <wiktor@mext.pl>
2016-03-01 11:51:47 +0100

    test(app): additional tests using fakeAsync() and TestComponentBuilder

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -3,7 +3,9 @@ import {
     inject,
     injectAsync,
     beforeEachProviders,
-    TestComponentBuilder
+    TestComponentBuilder,
+    fakeAsync,
+    tick
 } from 'angular2/testing';
 import {provide} from 'angular2/core';
 
@@ -21,12 +23,30 @@ describe('App component', () => {
         provide(WikipediaService, {useValue: mockWikipediaService}),
         App
     ]);
+    it('should show search bar', injectAsync([TestComponentBuilder],(tcb) => {
+        return tcb.createAsync(App).then((fixture) => {
+            fixture.detectChanges();
+            var compiled = fixture.debugElement.nativeElement;
+            expect(compiled.querySelector('h2').innerText === 'Wikipedia Search').toEqual(true);
+        });
+    }));
     it('should have empty item list at the beginning', inject( [App], (app) => {
         expect(app.items.length).toEqual(0);
     }));
-    it('should have non-empty list after search a term', inject( [App], (app) => {
-        app.search('test').then((response) => {
+    it('should have non-empty list after search a term', injectAsync( [App], (app) => {
+        return app.search('test').then((response) => {
             expect(app.items.length).toEqual(3);
         });
     }));
+    it('should display a list of found items', inject([TestComponentBuilder], fakeAsync((tcb) => {
+        var fixture;
+        tcb.createAsync(App).then((rootFixture) => {
+            fixture = rootFixture;
+        });
+        tick(); // Create component-tick (initializes fixture variable)
+        fixture.debugElement.componentInstance.items = ['Test1', 'Test2', 'Test3'];
+        fixture.detectChanges();
+        var compiled = fixture.debugElement.nativeElement;
+        expect(compiled.querySelector('ul').children.length).toEqual(3);
+    })));
 });
